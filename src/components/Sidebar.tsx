81ff56145dba4ec1f24e88796f86b1010ebe9593
K-taiga <taigakatou0329@gmail.com>
2026-07-21 11:20:59 +0900

    [update] ユーザーアイコンのファイル形式を `.webp` から `.png` に変更

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -17,7 +17,7 @@ const Sidebar = () => {
             </div>
             <div className="py-5 mt-auto mx-2 flex flex-col items-center">
                 <div className="bg-gray-700 p-2 rounded-lg">
-                    <img src={"/default-user-icon.webp"} alt=""/>
+                    <img src={"/default-user-icon.png"} alt=""/>
                 </div>
             </div>
         </div>
